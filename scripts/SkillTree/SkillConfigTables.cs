cf430bbc3fdea7fae45dc1ba0dea9391cf8ac460
RoarlisF/KP31 <roarlisf@gmail.com>
2024-07-29 15:49:06 -0700

    Psionic/Stealth Config

--- scripts/SkillTree/SkillConfigTables.cs
+++ scripts/SkillTree/SkillConfigTables.cs
@@ -389,6 +389,8 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.ProjectileConfig.SpoonThrowCooldown}s",
         IconPath = "AbilityIcon_Merged/psionic/spoon_throw.png",
         MaximumLevel = 1,
+        GridX = 1,
+        GridY = 0,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Psionic,
         UpgradeCost = 100,
@@ -409,6 +411,8 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.ProjectileConfig.BefuddleCooldown}s",
         IconPath = "AbilityIcon_Merged/psionic/befuddle.png",
         MaximumLevel = 1,
+        GridX = 0,
+        GridY = 1,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Psionic,
         UpgradeCost = 850,
@@ -429,13 +433,15 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.ProjectileConfig.PsyboltCooldown}s",
         IconPath = "AbilityIcon_Merged/psionic/psybolt.png",
         MaximumLevel = 1,
+        GridX = 1,
+        GridY = 1,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Psionic,
         UpgradeCost = 900,
         UIPosition = new Vector2(170, 230),
         SkillKey = "Psybolt",
         ParentNodeKeys = new string[] { "SpoonThrow" },
-        ChildrenNodeKeys = new string[] { "PsionicBeam", "PsyThrow" },
+        ChildrenNodeKeys = new string[] { "PsionicBeam" },
     };
 
     /// <summary>
@@ -447,6 +453,8 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.SelfHealConfig.Cooldown}s",
         IconPath = "AbilityIcon_Merged/psionic/self_heal.png",
         MaximumLevel = 1,
+        GridX = 2,
+        GridY = 1,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Psionic,
         UpgradeCost = 450,
@@ -465,6 +473,8 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.RegenerateConfig.Cooldown}s",
         IconPath = "AbilityIcon_Merged/psionic/regeneration.png",
         MaximumLevel = 1,
+        GridX = 2,
+        GridY = 2,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Psionic,
         UpgradeCost = 1350,
@@ -482,13 +492,15 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.HypnotizeConfig.Cooldown}s",
         IconPath = "AbilityIcon_Merged/psionic/hypnotize.png",
         MaximumLevel = 1,
+        GridX = 0,
+        GridY = 2,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Psionic,
         UpgradeCost = 1900,
         UIPosition = new Vector2(450, 430),
         SkillKey = "Hypnotize",
         ParentNodeKeys = new string[] { "Befuddle" },
-        ChildrenNodeKeys = new string[] { },
+        ChildrenNodeKeys = new string[] { "PsyThrow" },
 
     };
 
@@ -500,6 +512,8 @@ public static partial class SkillConfig
         RangeDescriptionKey = "3-7m",
         IconPath = "AbilityIcon_Merged/psionic/psionic_beam.png",
         MaximumLevel = 1,
+        GridX = 1,
+        GridY = 2,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Psionic,
         UpgradeCost = 2700,
@@ -518,12 +532,14 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.PsyThrowConfig.Cooldown}s",
         IconPath = "AbilityIcon_Merged/psionic/psythrow.png",
         MaximumLevel = 1,
+        GridX = 0,
+        GridY = 3,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Psionic,
         UpgradeCost = 3400,
         UIPosition = new Vector2(240, 430),
         SkillKey = "PsyThrow",
-        ParentNodeKeys = new string[] { "Psybolt" },
+        ParentNodeKeys = new string[] { "Hypnotize" },
         ChildrenNodeKeys = new string[] { },
 
     };
@@ -538,13 +554,37 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.InvisibilityConfig.Cooldown}s",
         IconPath = "AbilityIcon_Merged/stealth/invisibility.png",
         MaximumLevel = 1,
+        GridX = 1,
+        GridY = 0,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Stealth,
         UpgradeCost = 100,
         UIPosition = new Vector2(450, 30),
         SkillKey = "Invisibility",
         ParentNodeKeys = new string[] { },
-        ChildrenNodeKeys = new string[] { "LightFeet" },
+        ChildrenNodeKeys = new string[] { "LightFeet", "SpeedBoost", "Shuriken" },
+
+    };
+    
+    public static readonly SkillTreeNodeConfig SpeedBoostConfig = new SkillTreeNodeConfig()
+    {
+        DescriptionTextKey = "Increase Speed by 10% in Combat passively.",
+        IconPath = "AbilityIcon_Merged/stealth/speed_boost.png",
+        MaximumLevel = 1,
+        GridX = 0,
+        GridY = 1,
+        NType = NodeType.AttrBoost,
+        NTab = SkillTreeTabs.Stealth,
+        UpgradeCost = 100,
+        UIPosition = new Vector2(450, 30),
+        SkillKey = "SpeedBoost",
+        ParentNodeKeys = new string[] { "Invisibility" },
+        ChildrenNodeKeys = new string[] { "BearTrap" },
+        Buff = new StatBuff()
+        {
+            BoostType = StatType.BaseSpeed,
+            BoostValue = 8 // Your base speed is 80
+        }
 
     };
 
@@ -554,13 +594,15 @@ public static partial class SkillConfig
         IconPath = "AbilityIcon_Merged/stealth/light_feet.png",
         CooldownKey = $"{EffectConfig.LightFeetConfig.Cooldown}s",
         MaximumLevel = 1,
+        GridX = 2,
+        GridY = 1,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Stealth,
         UpgradeCost = 500,
         UIPosition = new Vector2(450, 230),
         SkillKey = "LightFeet",
         ParentNodeKeys = new string[] { "Invisibility" },
-        ChildrenNodeKeys = new string[] { "Shuriken", "ShadowStep" },
+        ChildrenNodeKeys = new string[] { "ShadowStep" },
 
     };
 
@@ -572,13 +614,15 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.ProjectileConfig.ShurikenCooldown}s",
         IconPath = "AbilityIcon_Merged/stealth/shuriken.png",
         MaximumLevel = 1,
+        GridX = 1,
+        GridY = 1,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Stealth,
         UpgradeCost = 850,
         UIPosition = new Vector2(450, 430),
         SkillKey = "Shuriken",
-        ParentNodeKeys = new string[] { "LightFeet" },
-        ChildrenNodeKeys = new string[] { "BearTrap", "Backstab" },
+        ParentNodeKeys = new string[] { "Invisibility" },
+        ChildrenNodeKeys = new string[] { "Backstab" },
     };
 
     public static readonly SkillTreeNodeConfig BearTrapConfig = new SkillTreeNodeConfig()
@@ -588,13 +632,15 @@ public static partial class SkillConfig
         RangeDescriptionKey = $"{EffectConfig.BearTrapConfig.MaxSetupDistance}m",
         CooldownKey = $"{EffectConfig.BearTrapConfig.Cooldown}s",
         MaximumLevel = 1,
+        GridX = 0,
+        GridY = 2,
         IconPath = "AbilityIcon_Merged/stealth/bear_trap.png",
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Stealth,
         UpgradeCost = 1650,
         UIPosition = new Vector2(450, 630),
         SkillKey = "BearTrap",
-        ParentNodeKeys = new string[] { "Shuriken" },
+        ParentNodeKeys = new string[] { "SpeedBoost" },
         ChildrenNodeKeys = new string[] { },
     };
 
@@ -605,6 +651,8 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.ShadowStepConfig.Cooldown}s",
         IconPath = "AbilityIcon_Merged/stealth/shadow_step.png",
         MaximumLevel = 1,
+        GridX = 2,
+        GridY = 2,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Stealth,
         UpgradeCost = 1500,
@@ -622,6 +670,8 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.BackStabConfig.Cooldown}s",
         IconPath = "AbilityIcon_Merged/stealth/backstab.png",
         MaximumLevel = 1,
+        GridX = 1,
+        GridY = 2,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Stealth,
         UpgradeCost = 2350,
@@ -637,6 +687,8 @@ public static partial class SkillConfig
         CooldownKey = $"{EffectConfig.TotalDarknessConfig.Cooldown}s",
         IconPath = "AbilityIcon_Merged/stealth/total_darkness.png",
         MaximumLevel = 1,
+        GridX = 2,
+        GridY = 3,
         NType = NodeType.SkillUnlock,
         NTab = SkillTreeTabs.Stealth,
         UpgradeCost = 3500,
@@ -710,6 +762,7 @@ public static partial class SkillConfig
             // Stealth
             { "Invisibility", InvisibilityConfig },
             { "LightFeet", LightFeetConfig },
+            {"SpeedBoost", SpeedBoostConfig},
             { "Shuriken", ShurikenConfig },
             { "BearTrap", BearTrapConfig },
             { "ShadowStep", ShadowStepConfig },
@@ -731,7 +784,7 @@ public static partial class SkillConfig
         // Psionic
         "SpoonThrow", "Befuddle", "Psybolt", "SelfHeal", "Regeneration", "Hypnotize", "PsionicBeam", "PsyThrow",
         // Stealth
-        "Invisibility", "LightFeet", "Shuriken", "BearTrap", "ShadowStep", "Backstab", "TotalDarkness",
+        "Invisibility", "LightFeet", "SpeedBoost","Shuriken", "BearTrap", "ShadowStep", "Backstab", "TotalDarkness",
         // Elemental
         "IceFist"
     };
