3dbaa249d79abbbf30ed537250e84f8f47da5867
joshua <joshuamarkk@gmail.com>
2024-07-29 19:13:58 -0700

    fix name being at the wrong offset

--- scripts/FightPlayerAnimation.cs
+++ scripts/FightPlayerAnimation.cs
@@ -8,8 +8,8 @@ public partial class FightPlayer
     // Create your state machine here. We are going to have a sh*t ton of animations so keep this section well documented.
     private void InitializeStateMachine()
     {
-        
-        SpineAnimator.SpineInstance.Scale = new Vector2(0.528f, 0.528f);
+        SpineAnimator.Entity.Scale = new Vector2(0.528f, 0.528f);
+        // SpineAnimator.SpineInstance.Scale = new Vector2(0.528f, 0.528f);
         
         var stateMachine = SpineAnimator.SpineInstance.StateMachine;
         var resetTrigger = stateMachine.TryGetVariableByName("RESET");
