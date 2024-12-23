c7bd5237f029efd65b0c09301c627580da6e1a06
RoarlisF/KP31 <fangzhx@shanghaitech.edu.cn>
2024-12-22 22:25:08 -0800

    Update FightPlayer.cs

--- scripts/FightPlayer.cs
+++ scripts/FightPlayer.cs
@@ -820,6 +820,11 @@ public partial class FightPlayer : Player
         return SkillTree;
     }
 
+    public CameraControl GetCameraInterface()
+    {
+        return CameraInterface;
+    }
+
     public FightPlayerSkillSlotsManager GetSkillSlots()
     {
         return SkillSlotsManager;
