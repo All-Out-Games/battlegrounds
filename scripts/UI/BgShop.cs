19d27b200beaf5ed6abd1b31a93ae358af69716f
RoarlisF/KP31 <zhixinfang@uvic.ca>
2025-02-28 14:32:32 -0800

    Case for access ticket

--- scripts/UI/BgShop.cs
+++ scripts/UI/BgShop.cs
@@ -73,6 +73,12 @@ public partial class BgShop : System<BgShop>
         {
             var prod = _allProducts.FirstOrDefault(prod => prod.SparksProductId == productId);
             success = true;
+
+            if (productId == "678014341902847169c60258")
+            {
+                return true; // This is the KoTH early access. Special case, should grant nothing but return true.
+            }
+            
             switch (prod.Id)
             {
                 case "starter_pack1": player.Exp += LevelingData.BaselineXp[4];
